24b3bf02a4747f334e1e458b1e8dde6dc863fc3e
loek <196374932+loek-sn@users.noreply.github.com>
2025-06-28 20:50:23 -0300

    Add variantProps type to fix typescript error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -22,6 +22,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { type VariantProps } from "class-variance-authority";
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state";
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7;
